263b5ce301088579e8c97e23ff21410f5de05f6b
independentvar <martynas.lobinas@gmail.com>
2016-05-09 14:12:32 +0200

    Basic square set classes added.

---
+++ KongOrange.Squares.DomainClasses/SquareSet.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace KongOrange.Squares.DomainClasses
+{
+    public class SquareSet
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<SquareSetPiece> Pieces { get; set; }
+    }
+}
---
+++ KongOrange.Squares.DomainClasses/SquareSetPiece.cs
@@ -0,0 +1,9 @@
+namespace KongOrange.Squares.DomainClasses
+{
+    public class SquareSetPiece
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
