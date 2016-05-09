a7ecba21e9eefe7b62a57a7e2e62894df45c3133
independentvar <martynas.lobinas@gmail.com>
2016-05-09 12:09:33 +0200

    Migrations enabled.

---
+++ KongOrange.Squares.DataAccess/Migrations/201605091004355_Initial.cs
@@ -0,0 +1,16 @@
+namespace KongOrange.Squares.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
