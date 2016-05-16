120130abcfea9448485b22ac70f082469e4dc925
independentvar <martynas.lobinas@gmail.com>
2016-05-16 12:38:17 +0200

    Added basic user administration.

---
+++ KongOrange.Squares.WebInterface/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,125 @@
+﻿using System.Data.Entity;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web.Mvc;
+using KongOrange.Squares.DataAccess;
+using KongOrange.Squares.DomainClasses;
+
+namespace KongOrange.Squares.WebInterface.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class UsersController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Admin/Users
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Users.ToListAsync());
+        }
+
+        // GET: Admin/Users/Details/5
+        public async Task<ActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ApplicationUser applicationUser = await db.Users.FirstAsync(o => o.Id == id);
+            if (applicationUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(applicationUser);
+        }
+
+        // GET: Admin/Users/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Users/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Email,EmailConfirmed,PasswordHash,SecurityStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEndDateUtc,LockoutEnabled,AccessFailedCount,UserName")] ApplicationUser applicationUser)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Users.Add(applicationUser);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(applicationUser);
+        }
+
+        // GET: Admin/Users/Edit/5
+        public async Task<ActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ApplicationUser applicationUser = await db.Users.FirstAsync(o => o.Id == id);
+            if (applicationUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(applicationUser);
+        }
+
+        // POST: Admin/Users/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Email,EmailConfirmed,PasswordHash,SecurityStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEndDateUtc,LockoutEnabled,AccessFailedCount,UserName")] ApplicationUser applicationUser)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(applicationUser).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(applicationUser);
+        }
+
+        // GET: Admin/Users/Delete/5
+        public async Task<ActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ApplicationUser applicationUser = await db.Users.FirstAsync(o => o.Id == id);
+            if (applicationUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(applicationUser);
+        }
+
+        // POST: Admin/Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(string id)
+        {
+            ApplicationUser applicationUser = await db.Users.FirstAsync(o => o.Id == id);
+            db.Users.Remove(applicationUser);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
