2892186383613da7f7ba629d5d8c88f4c740e3f7
Martynas Lobinas <martynas.lobinas@gmail.com>
2016-05-19 21:09:31 +0200

    Added browse page. Square set details page updated.

---
+++ KongOrange.Squares.WebInterface/Controllers/BrowseController.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using KongOrange.Squares.DataAccess;
+
+namespace KongOrange.Squares.WebInterface.Controllers
+{
+    public class BrowseController : Controller
+    {
+        private readonly ApplicationDbContext _db = new ApplicationDbContext();
+
+        // GET: Browse
+        public async Task<ActionResult> Index()
+        {
+            var squareSets = _db.SquareSets.Include(o => o.Pieces);
+            return View(await squareSets.ToListAsync());
+        }
+    }
+}
